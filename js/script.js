50952793227c72e24f2339271f448f46b905171b
ToiTenLaKhoa <anhkhoa.vv@gmail.com>
2022-11-22 22:17:49 +0700

    Remove unused lines of codes

--- js/script.js
+++ js/script.js
@@ -9,33 +9,16 @@ setInterval(() => {
     hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
-
-
-    // Digital Clock Code
     let hours = document.getElementById('hour');
     let minutes = document.getElementById('minutes');
     let seconds = document.getElementById('seconds');
-    let ampm = document.getElementById('ampm');
-
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-
-    // var am = h >= 12 ? 'PM' : 'AM'; 
-
-    //Convert 24 Hour Time To 12 Hour with AM PM Indicator
-    // if(h > 12){
-    // 		h = h - 12;
-    // }
-
-    // Add zero before Single Numbers
     h = (h < 10) ? "0" + h : h
     m = (m < 10) ? "0" + m : m
     s = (s < 10) ? "0" + s : s
-
-
     hours.innerHTML = h;
     minutes.innerHTML = m;
     seconds.innerHTML = s;
-    // ampm.innerHTML = am
-})  
+})
